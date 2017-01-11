1fdbaddcf7a0e5a8b9abd92a53d0cada76467f16
Shane Martin <shanemartin@Shanes-MBP-2.gateway>
2017-01-11 23:23:11 +1000

    Assets

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Stat
+{
+	public int value;
+}
+
+public class Stats
+{
+	public Stat damage;
+	public Stat health;
+	public Stat dexterity;
+	public Stat luck;
+}
